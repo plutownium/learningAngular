254183f2ee5b7038eb6d10c8653b20f61dc3091d
Roland Mackintosh <rolandmackintosh@outlook.com>
2022-01-12 23:00:58 -0800

    created 3 components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ViewComponentComponent } from './view-component/view-component.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ViewComponent } from './view/view.component';
+import { HeadlineComponent } from './headline/headline.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewComponentComponent,
+    DashboardComponent,
+    ViewComponent,
+    HeadlineComponent
   ],
   imports: [
     BrowserModule
