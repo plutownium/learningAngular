de7637302576d19d96ac556ebf33d8ef3e9bf824
Roland Mackintosh <rolandmackintosh@outlook.com>
2022-01-14 07:02:46 -0800

    child component displays

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output,EventEmitter } from '@angular/core';
+import { Customer } from './customer';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent implements OnInit {
+export class ChildComponent {
+  @Input() hero!: string;
+  @Input('master') masterName = ''; // tslint:disable-line: no-input-rename
+  customers: Customer[] = [
+ 
+    {customerNo: 1, name: 'Rahuld Dravid', address: '', city: 'Banglaore', state: 'Karnataka', country: 'India'},
+    {customerNo: 2, name: 'Sachin Tendulkar', address: '', city: 'Mumbai', state: 'Maharastra', country: 'India'},
+    {customerNo: 3, name: 'Saurrav Ganguly', address: '', city: 'Kolkata', state: 'West Bengal', country: 'India'},
+    {customerNo: 4, name: 'Mahendra Singh Dhoni', address: '', city: 'Ranchi', state: 'Bihar', country: 'India'},
+    {customerNo: 5, name: 'Virat Kohli', address: '', city: 'Delhi', state: 'Delhi', country: 'India'},
+ 
+  ]
 
-  constructor() { }
+  @Output() someMagicEvent = new EventEmitter<number>();
+  @Output() subtraction = new EventEmitter<number>();
 
-  ngOnInit(): void {
+  signalAddOne(sth: string) {
+    // TODO: let user use a number input to change from 1-5 (but not 6) to add per click
+    const convertedInteger: number = parseInt(sth, 10)
+    this.someMagicEvent.emit(convertedInteger);
   }
 
+  signalSubtract() {
+    this.subtraction.emit(); // always subtract 1 or 5 or sth
+  }
 }
