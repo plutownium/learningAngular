935514cebc8af3a89857f61858057da8e49060ea
Roland Mackintosh <rolandmackintosh@outlook.com>
2022-01-14 04:33:29 -0800

    installed material and several components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { NgChartsModule } from 'ng2-charts';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FeaturesComponent } from './features/features.component';
+
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { HeadlineComponent } from './headline/headline.component';
+import { HeadlineComponent } from './features/headline/headline.component';
 import { ChartComponent } from './chart/chart.component';
 import { ViewerComponent } from './viewer/viewer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuListItemComponent } from './features/ui/menu-list-item/menu-list-item.component';
+import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './child/child.component';
 
 @NgModule({
   declarations: [
@@ -15,11 +28,15 @@ import { ViewerComponent } from './viewer/viewer.component';
     DashboardComponent,
     HeadlineComponent,
     ChartComponent,
-    ViewerComponent
+    ViewerComponent,
+    MenuListItemComponent,
+    ParentComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule,
-    NgChartsModule
+    NgChartsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
